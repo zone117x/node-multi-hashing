18ac7c73552f869761a4325177e5d5a916e28755
Matt <zone117x@gmail.com>
2014-03-30 02:21:18 -0600

    removed build folder

--- build/Release/.deps/Release/obj.target/multihashing/scrypt.o.d
+++
@@ -1,4 +0,0 @@
-cmd_Release/obj.target/multihashing/scrypt.o := cc '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/root/.node-gyp/0.10.26/src -I/root/.node-gyp/0.10.26/deps/uv/include -I/root/.node-gyp/0.10.26/deps/v8/include  -fPIC -Wall -Wextra -Wno-unused-parameter -pthread -m64 -O2 -fno-strict-aliasing -fno-tree-vrp -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/multihashing/scrypt.o.d.raw  -c -o Release/obj.target/multihashing/scrypt.o ../scrypt.c
-Release/obj.target/multihashing/scrypt.o: ../scrypt.c ../scrypt.h
-../scrypt.c:
-../scrypt.h:

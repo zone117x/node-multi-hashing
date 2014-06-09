93929ceae0b3c7ba0970a1cef383f57e1b03d14e
wallet42 <wallet42@felixweis.com>
2014-06-09 19:39:08 +0200

    revert cryptonight_hash rename

--- cryptonight.c
+++ cryptonight.c
@@ -204,7 +204,7 @@ static inline void ExpandAESKey256(char *keybuf)
 	keys[14] = tmp1;
 }
 
-void cryptonight_hash_ctx(const void *input, void *output, uint32_t len)
+void cryptonight_hash(const void *input, void *output, uint32_t len)
 {
 	struct cryptonight_ctx *ctx = (struct cryptonight_ctx *)malloc(sizeof(struct cryptonight_ctx));
     hash_process(&ctx->state.hs, (const uint8_t*) input, 76);

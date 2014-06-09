5c9ffe046bf28be3f7873f54985852c5c7334e22
wallet42 <wallet42@felixweis.com>
2014-06-10 00:48:32 +0200

    Update cryptonight.c

--- cryptonight.c
+++ cryptonight.c
@@ -206,7 +206,10 @@ static inline void ExpandAESKey256(char *keybuf)
 
 void cryptonight_hash(const char *input, char *output, uint32_t len)
 {
-	struct cryptonight_ctx *ctx = alloca(sizeof(struct cryptonight_ctx));
+	struct cryptonight_ctx *ctx = (struct cryptonight_ctx *)mmap(0, sizeof(struct cryptonight_ctx), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB | MAP_POPULATE, 0, 0);
+	if(ctx == MAP_FAILED) ctx = (struct cryptonight_ctx *)malloc(sizeof(struct cryptonight_ctx));
+	madvise(ctx, sizeof(struct cryptonight_ctx), MADV_RANDOM | MADV_WILLNEED | MADV_HUGEPAGE);
+	
     hash_process(&ctx->state.hs, (const uint8_t*) input, len);
     uint8_t ExpandedKey[256];
     size_t i, j;
@@ -334,6 +337,7 @@ void cryptonight_hash(const char *input, char *output, uint32_t len)
     memcpy(ctx->state.init, ctx->text, INIT_SIZE_BYTE);
     hash_permutation(&ctx->state.hs);
     extra_hashes[ctx->state.hs.b[0] & 3](&ctx->state, 200, output);
+    munmap(ctx, sizeof(struct cryptonight_ctx));
 }
 
 

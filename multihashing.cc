8a4e442b3400dc28f17e0baa44303a7defeb110a
Matthew Little <zone117x@gmail.com>
2014-06-27 11:22:42 -0600

    Added initial x15 support

--- multihashing.cc
+++ multihashing.cc
@@ -20,7 +20,8 @@ extern "C" {
     #include "cryptonight.h"
     #include "x13.h"
     #include "nist5.h"
-    #include "sha1.h"
+    #include "sha1.h",
+    #include "x15.h"
 }
 
 #include "boolberry.h"
@@ -528,6 +529,28 @@ Handle<Value> sha1(const Arguments& args) {
     return scope.Close(buff->handle_);
 }
 
+Handle<Value> x15(const Arguments& args) {
+    HandleScope scope;
+
+    if (args.Length() < 1)
+        return except("You must provide one argument.");
+
+    Local<Object> target = args[0]->ToObject();
+
+    if(!Buffer::HasInstance(target))
+        return except("Argument should be a buffer object.");
+
+    char * input = Buffer::Data(target);
+    char output[32];
+
+    uint32_t input_len = Buffer::Length(target);
+
+    x15_hash(input, output, input_len);
+
+    Buffer* buff = Buffer::New(output, 32);
+    return scope.Close(buff->handle_);
+}
+
 void init(Handle<Object> exports) {
     exports->Set(String::NewSymbol("quark"), FunctionTemplate::New(quark)->GetFunction());
     exports->Set(String::NewSymbol("x11"), FunctionTemplate::New(x11)->GetFunction());
@@ -549,6 +572,7 @@ void init(Handle<Object> exports) {
     exports->Set(String::NewSymbol("boolberry"), FunctionTemplate::New(boolberry)->GetFunction());
     exports->Set(String::NewSymbol("nist5"), FunctionTemplate::New(nist5)->GetFunction());
     exports->Set(String::NewSymbol("sha1"), FunctionTemplate::New(sha1)->GetFunction());
+    exports->Set(String::NewSymbol("x15"), FunctionTemplate::New(x13)->GetFunction());
 }
 
 NODE_MODULE(multihashing, init)

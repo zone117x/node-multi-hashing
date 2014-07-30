358d118541c9e61bba352d755a14eaef12fefc8f
ahmedbodi <ahmed_bodi@msn.com>
2014-07-30 02:49:20 +0100

    Syntax

--- neoscrypt.h
+++ neoscrypt.h
@@ -1,7 +1,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-void neoscrypt(const uchar *password, uchar *output, uint profile) 
+void neoscrypt(const uchar *password, uchar *output, uint profile);
 #ifdef __cplusplus
 }
 #else

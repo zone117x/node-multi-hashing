a4a85b69b46f3ada2e63c1a5f93b318d338f1516
KryptosLab <admin@kryptoslab.com>
2014-10-25 06:14:04 +0200

    Adding dcrypt.h

---
+++ dcrypt.h
@@ -0,0 +1,16 @@
+#ifndef FRESH_H
+#define FRESH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void dcrypt_hash(const char*, char*, uint32_t);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

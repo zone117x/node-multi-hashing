686f9369a99a8e2fc5d78453f493783a6d725602
Excalibur201010 <sim201010@gmail.com>
2014-11-02 12:02:38 +0200

    Fix s3 algo

--- s3.c
+++ s3.c
@@ -11,23 +11,23 @@
 void s3_hash(const char* input, char* output, uint32_t len)
 {
     sph_shavite512_context      ctx_shavite1;
-    sph_simd512_context		ctx_simd1;
+    sph_simd512_context         ctx_simd1;
     sph_skein512_context        ctx_skein;
 
     //these uint512 in the c++ source of the client are backed by an array of uint32
     uint32_t hashA[16], hashB[16];
 
     sph_shavite512_init (&ctx_shavite1);
-    sph_shavite512 (&ctx_shavite1, hashB, 64);   
+    sph_shavite512 (&ctx_shavite1, input, 80);
     sph_shavite512_close(&ctx_shavite1, hashA);
 
-    sph_simd512_init (&ctx_simd1); 
-    sph_simd512 (&ctx_simd1, hashA, 64);   
-    sph_simd512_close(&ctx_simd1, hashB); 
+    sph_simd512_init (&ctx_simd1);
+    sph_simd512 (&ctx_simd1, hashA, 64);
+    sph_simd512_close(&ctx_simd1, hashB);
 
     sph_skein512_init(&ctx_skein);
-    sph_skein512 (&ctx_skein, hashA, 64);
-    sph_skein512_close (&ctx_skein, hashB);
+    sph_skein512 (&ctx_skein, hashB, 64);
+    sph_skein512_close (&ctx_skein, hashA);
 
     memcpy(output, hashA, 32);
 }

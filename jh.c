f4383724e60d861ae6dd9f6358c1cadf40063a11
Ahmed Bodiwala <ahmedbodi@Ahmeds-MacBook-Pro.local>
2014-11-14 18:51:57 +0000

    Removed dependency on uint256

--- jh.c
+++ jh.c
@@ -11,14 +11,9 @@
 void jh_hash(const char* input, char* output, uint32_t len) {
 
     sph_jh256_context ctx_jh;
-    static unsigned char pblank[1];
-    uint256 hash;
-
     sph_jh256_init(&ctx_jh);
-    sph_jh256 (&ctx_jh, (pbegin == pend ? pblank : static_cast<const void*>(&pbegin[0])), (pend - pbegin) * sizeof(pbegin[0]));
-    sph_jh256_close(&ctx_jh, static_cast<void*>(&hash));
-
-    return hash;
+    sph_jh256 (&ctx_jh, input, len);
+    sph_jh256_close(&ctx_jh, output);
 
 }
 

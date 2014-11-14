99926eb1f4c689053295f0935b0dc1cafe7b6806
Ahmed Bodiwala <ahmedbodi@Ahmeds-MacBook-Pro.local>
2014-11-14 13:24:36 +0000

    Add JH Files

---
+++ jha.h
@@ -0,0 +1,16 @@
+#ifndef JHA_H
+#define JHA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void jha_hash(const char* input, char* output, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

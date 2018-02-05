7d8cb9219a88db7e39d3e4e9ef8859405c4ade5e
Andrey Demenev <demenev@gmail.com>
2018-02-05 17:40:57 +1000

    Remove extra comma

--- multihashing.cc
+++ multihashing.cc
@@ -20,7 +20,7 @@ extern "C" {
     #include "cryptonight.h"
     #include "x13.h"
     #include "nist5.h"
-    #include "sha1.h",
+    #include "sha1.h"
     #include "x15.h"
 	#include "fresh.h"
 }

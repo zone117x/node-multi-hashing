09f2507e62c8a8d48c269be1bc68bffc4e66a248
Julian Haupt <julian.haupt@hauptmedia.de>
2018-10-29 18:51:32 +0100

    add lbry algorithm (#68)

---
+++ lbry.h
@@ -0,0 +1,14 @@
+#ifndef LBRY_H
+#define LBRY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+    void lbry_hash(const char* input, char* output, uint32_t len);
+#ifdef __cplusplus
+}
+#endif
+
+#endif

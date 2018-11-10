f76ebf6593e5a8b7b5b69cc16bce6a228a54e4fd
James Lovejoy <jameslovejoy1@gmail.com>
2018-11-09 21:33:05 -0500

    Lyra2REv3
    Please see https://github.com/metalicjames/lyra2re-hash-python/pull/5

--- lyra2re.c
+++ lyra2re.c
@@ -110,3 +110,31 @@ void lyra2rev2_hash(const char* input, char* output, uint32_t _len)
 
    	memcpy(output, hashA, 32);
 }
+
+void lyra2rev3_hash(const char* input, char* output, uint32_t _len)
+{
+    sph_blake256_context ctx_blake;
+    sph_cubehash256_context ctx_cubehash;
+
+    sph_bmw256_context ctx_bmw;
+
+    uint32_t hashA[8], hashB[8];
+
+    sph_blake256_init(&ctx_blake);
+    sph_blake256(&ctx_blake, input, 80);
+    sph_blake256_close (&ctx_blake, hashA);
+
+    LYRA2_3(hashB, 32, hashA, 32, hashA, 32, 1, 4, 4);
+
+    sph_cubehash256_init(&ctx_cubehash);
+    sph_cubehash256(&ctx_cubehash, hashB, 32);
+    sph_cubehash256_close(&ctx_cubehash, hashA);
+
+    LYRA2_3(hashB, 32, hashA, 32, hashA, 32, 1, 4, 4);
+
+    sph_bmw256_init(&ctx_bmw);
+    sph_bmw256(&ctx_bmw, hashB, 32);
+    sph_bmw256_close(&ctx_bmw, hashA);
+
+    memcpy(output, hashA, 32);
+}
--- lyra2re.h
+++ lyra2re.h
@@ -9,6 +9,7 @@ extern "C" {
 
 void lyra2re_hash(const char* input, char* output, uint32_t _len);
 void lyra2rev2_hash(const char* input, char* output, uint32_t _len);
+void lyra2rev3_hash(const char* input, char* output, uint32_t _len);
 
 #ifdef __cplusplus
 }

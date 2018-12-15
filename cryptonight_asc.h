dc873b92ef25bce76a2c5898479074d2e8ff8765
Cideg <34962099+Cideg@users.noreply.github.com>
2018-12-15 18:31:47 +0100

    Update and rename cryptonight_fast.h to cryptonight_asc.h

---
+++ cryptonight_asc.h
@@ -0,0 +1,17 @@
+#ifndef CRYPTONIGHTASC_H
+#define CRYPTONIGHTASC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+void cryptonightasc_hash(const char* input, char* output, uint32_t len, int variant);
+void cryptonightasc_fast_hash(const char* input, char* output, uint32_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

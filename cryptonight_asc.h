45cbd5ca2d94c46de7cdd73f7377f8fd18e2aa3d
Cideg <34962099+Cideg@users.noreply.github.com>
2018-12-18 14:06:25 +0100

    Update cryptonight_asc.h

--- cryptonight_asc.h
+++ cryptonight_asc.h
@@ -8,7 +8,7 @@ extern "C" {
 #include <stdint.h>
 
 void cryptonightasc_hash(const char* input, char* output, uint32_t len, int variant);
-void cryptonightasc_fast_hash(const char* input, char* output, uint32_t len);
+void cryptonightasc_asc_hash(const char* input, char* output, uint32_t len);
 
 #ifdef __cplusplus
 }

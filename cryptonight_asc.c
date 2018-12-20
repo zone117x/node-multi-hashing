c2e26f3626478e3394d4ccb383de5823a4d88588
Cideg <34962099+Cideg@users.noreply.github.com>
2018-12-20 20:35:02 +0100

    Update cryptonight_asc.c

--- cryptonight_asc.c
+++ cryptonight_asc.c
@@ -175,23 +175,23 @@ void cryptonightfast_hash(const char* input, char* output, uint32_t len, int var
 
     for (i = 0; i < ITER / 4; i++) {
       j = e2i(ctx->a, MEMORY / AES_BLOCK_SIZE) * AES_BLOCK_SIZE);
-      copy_block(c, &long_state[j]); //Copying the block the pointer points to accessable cache (c1)
-      copy_block(c1, &long_state[j]); //Copying the block the pointer points to accessable cache (c2)
+      copy_block(ctx->c, &long_state[j]); //Copying the block the pointer points to accessable cache (c1)
+      copy_block(ctx->c1, &long_state[j]); //Copying the block the pointer points to accessable cache (c2)
       /* Iteration 0 */
-      aesb_single_round(c, c, a); //AES of c1 to c1. key: a
-      copy_block(&long_state[j], c); // Copying encrypted block back
+      aesb_single_round(ctx->c,ctx-> c,ctx-> a); //AES of c1 to c1. key: a
+      copy_block(&long_state[j],ctx-> c); // Copying encrypted block back
       /* Iteration 1 */
-      j = e2i(c, MEMORY / AES_BLOCK_SIZE) * AES_BLOCK_SIZE;
-      xor_blocks(c, b); //XOR Block with another thing
-      copy_block(&long_state[j], c);
+      j = e2i(ctx->c, MEMORY / AES_BLOCK_SIZE) * AES_BLOCK_SIZE;
+      xor_blocks(ctx->c, ctx->b); //XOR Block with another thing
+      copy_block(&long_state[j], ctx->c);
       /* Iteration 2 */
       j = e2i(c, MEMORY / AES_BLOCK_SIZE) * AES_BLOCK_SIZE;
-      copy_block(&long_state[j], c1); // Copying previous block back to random position
-      xor_blocks(c1, c); //XORing previous block with current block in pos
+      copy_block(&long_state[j],ctx-> c1); // Copying previous block back to random position
+      xor_blocks(ctx->c1, ctx->c); //XORing previous block with current block in pos
 
       /* Iteration 3 */
       j = e2i(c1, MEMORY / AES_BLOCK_SIZE) * AES_BLOCK_SIZE;
-      copy_block(&long_state[j], c1); // Copying XORed block to random pos ([C1 after encryption XOR B] XOR C1 before encryption)
+      copy_block(&long_state[j],ctx-> c1); // Copying XORed block to random pos ([C1 after encryption XOR B] XOR C1 before encryption)
 
       /* Finishing */
       mul(c, c1, d);
